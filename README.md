ecc7f31b9d9ea0678aba84161abceb837a3d67c2
junxi.wjd <junxi.wjd@cainiao.com>
2018-08-16 23:19:53 +0800

    init

---
+++ README.md
@@ -0,0 +1,2 @@
+# venus
+Venus strives to be the best blogging system. The brightest star in the sky before the darkest dawn! Have the meaning of guidance!
---
+++ src/main/java/com/colinwjd/venus/VenusApplication.java
@@ -0,0 +1,12 @@
+package com.colinwjd.venus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VenusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VenusApplication.class, args);
+	}
+}

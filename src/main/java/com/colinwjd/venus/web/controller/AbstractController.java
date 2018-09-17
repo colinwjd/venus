11c9c9436d48285397dec99861cf8900e6646a09
junxi.wjd <junxi.wjd@cainiao.com>
2018-09-17 14:30:14 +0800

    add error controller

--- src/main/java/com/colinwjd/venus/web/controller/AbstractController.java
+++ src/main/java/com/colinwjd/venus/web/controller/AbstractController.java
@@ -1,6 +1,10 @@
 package com.colinwjd.venus.web.controller;
 
 import org.springframework.boot.web.servlet.error.ErrorController;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 /**
  * 抽象控制器
@@ -12,16 +16,27 @@ public abstract class AbstractController implements ErrorController {
 
     protected static String THEME = "default";
 
-    private static String ERROR_PATH = "/error";
+    private static final String ERROR_PATH = "/error";
 
     /**
-     * 主题渲染
+     * 主题模板路径渲染
      *
      * @param name 视图名称
      * @return 主题对应的视图
      */
     protected String render(String name) {
         return "themes/" + THEME + "/" + name;
+    }
+
+    @RequestMapping(ERROR_PATH)
+    public String errorHandler(HttpServletRequest request, HttpServletResponse response) {
+        Integer statusCode = (Integer) request.getAttribute("javax.servlet.error.status_code");
+        switch (statusCode) {
+            case 404:
+                return this.render("404");
+            default:
+                return this.render("500");
+        }
     }
 
     @Override

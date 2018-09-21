734a8340ba827943c383f60a3cb333273fb434f2
Colin Wang <wjdwjd@live.cn>
2018-09-21 21:40:33 +0800

    add http utils

--- src/main/java/com/colinwjd/venus/common/util/HttpUtil.java
+++ src/main/java/com/colinwjd/venus/common/util/HttpUtil.java
@@ -1,11 +1,13 @@
 package com.colinwjd.venus.common.util;
 
 import java.io.IOException;
+import java.net.URISyntaxException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.collections4.MapUtils;
 import org.apache.http.Consts;
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
@@ -15,6 +17,9 @@ import org.apache.http.client.HttpClient;
 import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
+import org.apache.http.client.methods.HttpUriRequest;
+import org.apache.http.client.utils.URIBuilder;
+import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.HttpClients;
 import org.apache.http.message.BasicNameValuePair;
 import org.apache.http.util.EntityUtils;
@@ -32,28 +37,60 @@ public final class HttpUtil {
 
     public static String get(String url) {
         HttpGet httpGet = new HttpGet(url);
-        String result = null;
-        try {
-            HttpResponse response = HTTP_CLIENT.execute(httpGet);
-            result = EntityUtils.toString(response.getEntity());
-        } catch (ParseException | IOException e) {
-            log.warn("Http get exception: ", e);
-        }
-        return result;
+        return execute(httpGet);
+    }
+
+    public static String get(String url, Map<String, String> params) {
+        HttpGet httpGet = new HttpGet(buildUri(url, params));
+        return execute(httpGet);
+    }
+
+    public static String post(String url) {
+        HttpPost httpPost = new HttpPost(url);
+        return execute(httpPost);
     }
 
     public static String post(String url, Map<String, String> params) {
-        List<NameValuePair> pairs = new ArrayList<>();
-        params.forEach((k, v) -> pairs.add(new BasicNameValuePair(k, v)));
-        HttpEntity entity = new UrlEncodedFormEntity(pairs, Consts.UTF_8);
+        HttpEntity entity = new UrlEncodedFormEntity(buildParams(params), Consts.UTF_8);
         HttpPost httpPost = new HttpPost(url);
         httpPost.setEntity(entity);
+        return execute(httpPost);
+    }
+
+    public static String post(String url, String json) {
+        HttpPost httpPost = new HttpPost(url);
+        httpPost.addHeader("Content-Type", "application/json");
+        httpPost.setEntity(new StringEntity(json, Consts.UTF_8));
+        return execute(httpPost);
+    }
+
+    private static List<NameValuePair> buildParams(Map<String, String> params) {
+        List<NameValuePair> pairs = new ArrayList<>(MapUtils.size(params));
+        if (MapUtils.isNotEmpty(params)) {
+            params.forEach((k, v) -> pairs.add(new BasicNameValuePair(k, v)));
+        }
+        return pairs;
+    }
+
+    private static String buildUri(String url, Map<String, String> params) {
+        String uri = null;
+        try {
+            URIBuilder uriBuilder = new URIBuilder(url);
+            uriBuilder.addParameters(buildParams(params));
+            uri = uriBuilder.build().toString();
+        } catch (URISyntaxException e) {
+            log.warn("URI build exception: ", e);
+        }
+        return uri;
+    }
+
+    private static String execute(HttpUriRequest request) {
         String result = null;
         try {
-            HttpResponse response = HTTP_CLIENT.execute(httpPost);
+            HttpResponse response = HTTP_CLIENT.execute(request);
             result = EntityUtils.toString(response.getEntity());
-        } catch (ParseException | IOException e) {
-            log.warn("Http post exception: ", e);
+        } catch (IOException | ParseException e) {
+            log.warn("Http request exception: ", e);
         }
         return result;
     }
